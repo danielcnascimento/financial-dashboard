26353dc663ea53d7d085088bcdb8ca8f365ff78a
ddaniel.c_nascimento <ddaniel.nascimento@gmail.com>
2024-01-15 19:01:28 -0300

    Adding Config-base metadata.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
 
 /**
  * This is a Page that is inside a segment page, basically, a "child page of a child page",
