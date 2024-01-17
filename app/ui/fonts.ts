d0064ac6fa6fa2ac467720d3a8559da03e88ba23
ddaniel.c_nascimento <ddaniel.nascimento@gmail.com>
2024-01-16 22:08:03 -0300

    adding documentation

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,6 @@
 import { Inter, Lusitana } from 'next/font/google';
 
+/** Global font */
 export const inter = Inter({ subsets: ['latin'] });
+/** Secondary font used on Main page paragraph */
 export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
